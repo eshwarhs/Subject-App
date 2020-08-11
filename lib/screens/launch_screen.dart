4a898d6f78dc64260ddf9e03092b34e2ad427c11
vaibhavkt <vaibhavkt12@gmail.com>
2020-08-11 15:36:37 +0530

    Added first screen

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatelessWidget {
+
+  static const routeName='/launch';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+      
+    );
+  }
+}
